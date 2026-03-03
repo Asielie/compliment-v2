70c9443058ae312f39547f7113d6bf9dacf84ecb
Asielie <aseliekuotsu111@gmail.com>
2026-03-04 00:33:29 +0530

    Update compliments

--- src/components/screens/ComplimentsScreen.jsx
+++ src/components/screens/ComplimentsScreen.jsx
@@ -37,17 +37,17 @@ export default function ComplimentsScreen({ onNext }) {
             <div className="relative w-full max-w-md h-96 flex items-center justify-center">
                 <div className="absolute top-0 max-[366px]:left-0 left-3 md:left-14 z-10 -rotate-6">
                     <Card
-                        text="Being around you makes even ordinary days feel special."
+                        text="I love your hair, your eyes, your aesthetic."
                     />
                 </div>
                 <div className="absolute top-3 md:top-4 max-[366px]:right-0 right-4 md:right-14 z-20 rotate-12">
                     <Card
-                        text="I love how your eyes light up when you talk about things you love."
+                        text="Your pink guitar, I'd love to collab with you someday even though I suck at singing."
                     />
                 </div>
                 <div className="absolute bottom-4 left-1/2 -translate-x-1/2 z-30 -rotate-2">
                     <Card
-                        text="Your laugh is literally my favorite sound in the entire universe. Never stop."
+                        text="Your voice is literally my favorite sound in the entire universe. Never stop."
                     />
                 </div>
             </div>
